d946e76085f91b2f8dbeddae537e1942e78c46d1
Panagiotis Panagi <panayi1954@gmail.com>
2011-04-12 18:50:45 +0300

    Eshop Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eshop::Application.config.secret_token = '2db45179c17903b26d329d0e8fd5c3c4f8ea02a3e6c51fe58bf9f6fd74a072a425df4af3a9b75abe81f1f5ca266837df1cbb0742b42981c99d80c9f149ae6f5d'
