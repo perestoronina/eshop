b1c447eb8f7365d2672959c6900ae556566f6f41
Panagiotis Panagi <panayi1954@gmail.com>
2011-04-12 20:44:38 +0300

    Finished iteration D1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110412131024) do
+ActiveRecord::Schema.define(:version => 20110412173913) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
