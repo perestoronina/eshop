8275e090a12257920327357b623e38d8a9444320
Panagiotis Panagi <panayi1954@gmail.com>
2011-04-14 13:01:04 +0300

    Finished iteration I4

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,6 +39,14 @@ class User < ActiveRecord::Base
     end
   end
   
+  after_destroy :ensure_an_admin_remains
+
+  def ensure_an_admin_remains
+    if User.count.zero?
+      raise "Can't delete last user"
+    end
+  end     
+
   private
 
     def password_must_be_present
