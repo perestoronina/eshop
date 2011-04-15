d93f7689944af4953a4b8201677bb6425783b844
panayi <panayi1954@gmail.com>
2011-04-15 11:48:12 +0300

    no_table_pluralize

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ActiveRecord::Base.pluralize_table_names = false
+
 # Initialize the rails application
 Eshop::Application.initialize!
