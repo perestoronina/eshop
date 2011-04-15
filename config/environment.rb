49a4b0fc26972d2b0fe9a87bcbfc654fda2a5af9
panayi <panayi1954@gmail.com>
2011-04-15 11:50:14 +0300

    messed_up

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-ActiveRecord::Base.pluralize_table_names = false
 
 # Initialize the rails application
 Eshop::Application.initialize!
